5dbbe27cbbae0354b500854fb474f782c1f0ec3c
= <R139591066@taalim.ma>
2025-11-29 18:39:02 +0100

    editing motion effect by adding pathname

--- src/app/template.jsx
+++ src/app/template.jsx
@@ -1,10 +1,14 @@
 "use client";
 
 import { motion } from "framer-motion";
+import { usePathname } from "next/navigation";
 
 export default function Template({ children }) {
+  const pathname = usePathname();
+
   return (
-     <motion.div
+    <motion.div
+      key={pathname} // Force la réanimation à chaque changement de route
       initial={{ opacity: 0, y: 20 }}
       animate={{ opacity: 1, y: 0 }}
       exit={{ opacity: 0, y: -20 }}
